42ebfa3e2fbc12b66434df918f8f73bb1b3cde7c
KolRah <117634752+KolRah@users.noreply.github.com>
2023-07-11 10:31:39 +0200

    Create SolaceService.cs

---
+++ SolaceService.cs
@@ -0,0 +1,37 @@
+ public List<string> BrowseQueue(string queuename)
+        {
+            try
+            {   
+                List<string> RESULT = new List<string>();
+
+                ContextFactoryProperties cfp = new ContextFactoryProperties()
+                {
+                    SolClientLogLevel = SolLogLevel.Warning
+                };
+                cfp.LogToConsoleError();
+                _instance = ContextFactory.Instance;
+                _instance.Init(cfp);
+
+                using (IContext context = _instance.CreateContext(new ContextProperties(), null))
+                {
+                    QueueBrowser browser = new QueueBrowser()
+                    {
+                        VPNName = _parameters._VPN_NAME,
+                        UserName = _parameters._USERNAME,
+                        Password = _parameters._PASSWORD
+                    };
+                    RESULT = browser.Run(queuename,context, _parameters._HOST);
+                }
+                _instance.Cleanup();
+                return RESULT;
+            }
+            catch (Exception ex)
+            {
+                _instance.Cleanup();
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+                throw new SolaceConnectionException();
+            }
+            Console.WriteLine("Finished.");
+            
+        }
+    }
